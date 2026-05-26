05cd11aac88d679863ad30e6af21cc5396587b5a
Joetttttt <wengwohph@gmail.com>
2026-05-26 04:34:09 -0700

    auto update

--- index.php
+++ index.php
@@ -138,7 +138,7 @@
             font-weight: 500;
             animation: fadeInDown 0.3s ease;
             position: fixed;
-            width: 1;
+            width: 10;
         }
 
         .error-msg svg {
