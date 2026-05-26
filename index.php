431a92b4cd4472813dab9b81976c399ce0cd54d4
Joetttttt <wengwohph@gmail.com>
2026-05-26 04:36:47 -0700

    auto update

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
         /* Fallback SVG crest if image not available */
         
         .card-body-inner {
-            padding: 100px 16px 36px;
+            padding: 100px 16px 3px;
             margin: 0 50p;
         }
 
