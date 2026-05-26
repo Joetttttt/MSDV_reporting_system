29acc9a8845a48234f8e38b31b4014cb3f638c95
Joetttttt <wengwohph@gmail.com>
2026-05-26 05:16:27 -0700

    auto update

--- auth/logout.php
+++ auth/logout.php
@@ -10,7 +10,7 @@ header("Cache-Control: no-cache, no-store, must-revalidate");
 header("Pragma: no-cache");
 header("Expires: 0");
 
-header("Location: ../index");
+header("Location: ../inde");
 exit();
 
 ?>
