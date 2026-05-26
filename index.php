44797f06c6a2b7fb13f1fe763bad79255688fbdf
Joetttttt <wengwohph@gmail.com>
2026-05-26 04:08:26 -0700

    auto update

--- index.php
+++ index.php
@@ -188,7 +188,7 @@
     background: #091a47;
     border-radius: 50%;
       position: fixed;
-    transform: translate(0%, -55%);
+    transform: translate(60%, -55%);
     z-index: 9999;
 }
     </style>
