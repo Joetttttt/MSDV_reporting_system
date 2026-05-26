5bd433ba7b6e23743e7b6b9c4df3e04f536798d2
Joetttttt <wengwohph@gmail.com>
2026-05-26 04:28:30 -0700

    auto update

--- index.php
+++ index.php
@@ -136,7 +136,7 @@
             color: #9b1222;
             font-size: 13px;
             font-weight: 500;
-            animation: fadeInDow 0.3s ease;
+            animation: fadeInDo 0.3s ease;
             position: fixed;
         }
 
