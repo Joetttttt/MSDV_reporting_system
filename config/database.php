1a8a70c7cfa0b583b8ee49139294c3df9e68c35b
Joetttttt <wengwohph@gmail.com>
2026-05-27 02:59:57 -0700

    auto update

--- config/database.php
+++ config/database.php
@@ -3,7 +3,20 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "mcc_discipline_system";
+$db = "mcc_discipline_system";<?php
+
+$conn = mysqli_connect(
+    getenv("MYSQLHOST"),
+    getenv("MYSQLUSER"),
+    getenv("MYSQLPASSWORD"),
+    getenv("MYSQLDATABASE"),
+    getenv("MYSQLPORT")
+);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
