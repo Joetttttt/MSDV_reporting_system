728bac1d63bd236ed6a83ee7b76b205dc775d238
Joetttttt <wengwohph@gmail.com>
2026-05-28 12:45:02 -0700

    auto update

--- mcc_discipline_system.sql
+++ mcc_discipline_system.sql
@@ -88,7 +88,7 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('admin','teacher','csu','jassu') NOT NULL,
+  `role` enum('admin','teacher','csu','jassu','student') NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
