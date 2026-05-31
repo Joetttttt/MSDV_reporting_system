7cd0f51ad59e70ec22b061a37f6993064277dbd4
Joetttttt <wengwohph@gmail.com>
2026-05-31 00:23:16 -0700

    auto update

--- auth/update_password.php
+++ auth/update_password.php
@@ -4,43 +4,45 @@ session_start();
 
 include("../config/database.php");
 
-if (!isset($_SESSION['user_id'])) {
+if(!isset($_SESSION['user_id'])){
     header("Location: login.php");
     exit();
 }
 
-$new_password = $_POST['new_password'];
-$confirm_password = $_POST['confirm_password'];
+$new_password =
+    $_POST['new_password'];
 
-if ($new_password != $confirm_password) {
+$confirm_password =
+    $_POST['confirm_password'];
+
+if($new_password != $confirm_password){
 
     die("Passwords do not match.");
 
 }
 
-$user_id = $_SESSION['user_id'];
+$user_id =
+    $_SESSION['user_id'];
 
-$password = md5($new_password);
+$password =
+    md5($new_password);
 
-$sql = "UPDATE users
-        SET password='$password',
-            first_login=0
-        WHERE id='$user_id'";
+$sql =
+"UPDATE users
+ SET password='$password',
+ first_login=0
+ WHERE id='$user_id'";
 
-if (mysqli_query($conn, $sql)) {
+if(mysqli_query($conn,$sql)){
 
     session_destroy();
 
     echo "
     <script>
-        alert('Password changed successfully. Please login again.');
+        alert('Password changed successfully. Login again.');
         window.location='login.php';
     </script>
     ";
-
-} else {
-
-    echo "Error updating password.";
 
 }
 ?>
