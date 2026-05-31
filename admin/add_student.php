b32e6885d641f5c3d71e7f7816dc4aea50e2242e
Joetttttt <wengwohph@gmail.com>
2026-05-31 08:22:22 -0700

    auto update

--- admin/add_student.php
+++ admin/add_student.php
@@ -34,33 +34,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $encrypted_password = md5($default_password);
 
     mysqli_query($conn,"
-    INSERT INTO users
-    (
-        fullname,
-        username,
-        email,
-        password,
-        role,
-        student_id,
-        first_login
-    )
-    VALUES
-    (
-        '$fullname',
-        '$username',
-        '',
-        '$encrypted_password',
-        'student',
-        '$student_id',
-        1
-    )
-");
+        INSERT INTO users
+        (
+            fullname,
+            username,
+            email,
+            password,
+            role,
+            student_id,
+            first_login
+        )
+        VALUES
+        (
+            '$fullname',
+            '$username',
+            '',
+            '$encrypted_password',
+            'student',
+            '$student_id',
+            1
+        )
+    ");
 
-echo "
-<script>
-alert(
-'Student Added Successfully\n\nUsername: $username\nPassword: $default_password'
-);
-window.location='students.php';
-</script>";
-exit();
+    header("Location: students.php");
+    exit();
+}
+?>
