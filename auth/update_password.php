0ae81389ca14b26094051693e62d7d8f58a18e73
Joetttttt <wengwohph@gmail.com>
2026-05-31 00:12:54 -0700

    auto update

---
+++ auth/update_password.php
@@ -0,0 +1,46 @@
+<?php
+
+session_start();
+
+include("../config/database.php");
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$new_password = $_POST['new_password'];
+$confirm_password = $_POST['confirm_password'];
+
+if ($new_password != $confirm_password) {
+
+    die("Passwords do not match.");
+
+}
+
+$user_id = $_SESSION['user_id'];
+
+$password = md5($new_password);
+
+$sql = "UPDATE users
+        SET password='$password',
+            first_login=0
+        WHERE id='$user_id'";
+
+if (mysqli_query($conn, $sql)) {
+
+    session_destroy();
+
+    echo "
+    <script>
+        alert('Password changed successfully. Please login again.');
+        window.location='login.php';
+    </script>
+    ";
+
+} else {
+
+    echo "Error updating password.";
+
+}
+?>
