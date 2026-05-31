91e1b89d21ae5c4935ea0373e708e39b8a7374a4
Joetttttt <wengwohph@gmail.com>
2026-05-31 04:56:21 -0700

    auto update

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,81 +1,77 @@
 <div class="bg-dark text-white vh-100 p-3"
-style="width:280px;">
+style="width:280px; position:fixed;">
 
-<h4 class="text-center">
-MSDV Admin
-</h4>
+    <h4 class="text-center mb-4">
+        MSDV Admin
+    </h4>
 
-<hr>
+    <hr>
 
-<ul class="nav flex-column">
+    <ul class="nav flex-column">
 
-<li class="nav-item">
-<a href="dashboard.php"
-class="nav-link text-white">
-Dashboard
-</a>
-</li>
+        <li class="nav-item mb-2">
+            <a href="dashboard.php"
+               class="nav-link text-white">
+               Dashboard
+            </a>
+        </li>
 
-<li class="nav-item">
-<a href="student_records.php"
-class="nav-link text-white">
-Student Records
-</a>
-</li>
+        <li class="nav-item mb-2">
+            <a href="student_records.php"
+               class="nav-link text-white">
+               Student Records
+            </a>
+        </li>
 
-<li class="nav-item">
-<a href="violation_records.php"
-class="nav-link text-white">
-Violation Records
-</a>
-</li>
+        <li class="nav-item mb-2">
+            <a href="violation_records.php"
+               class="nav-link text-white">
+               Violation Records
+            </a>
+        </li>
 
-<li class="nav-item">
-<a href="disciplinary_actions.php"
-class="nav-link text-white">
-Disciplinary Actions
-</a>
-</li>
+        <li class="nav-item mb-2">
+            <a href="disciplinary_actions.php"
+               class="nav-link text-white">
+               Disciplinary Actions
+            </a>
+        </li>
 
-<li class="nav-item">
-<a href="risk_levels.php"
-class="nav-link text-white">
-Risk Levels
-</a>
-</li>
+        <li class="nav-item mb-2">
+            <a href="risk_levels.php"
+               class="nav-link text-white">
+               Risk Level Indicator
+            </a>
+        </li>
 
-<li class="nav-item">
-<a href="student_appeals.php"
-class="nav-link text-white">
-Student Appeals
-</a>
-</li>
+        <li class="nav-item mb-2">
+            <a href="student_appeals.php"
+               class="nav-link text-white">
+               Student Appeals
+            </a>
+        </li>
 
-<li class="nav-item">
-<a href="backup_export.php"
-class="nav-link text-white">
-Backup & Export
-</a>
-</li>
+        <li class="nav-item mb-2">
+            <a href="backup_export.php"
+               class="nav-link text-white">
+               Backup & Export
+            </a>
+        </li>
 
-<li class="nav-item">
-<a href="user_management.php"
-class="nav-link text-white">
-User Management
-</a>
-</li>
+        <li class="nav-item mb-2">
+            <a href="user_management.php"
+               class="nav-link text-white">
+               User Management
+            </a>
+        </li>
 
-<li class="nav-item mt-4">
+        <li class="nav-item mt-4">
+            <a href="../logout.php"
+               class="btn btn-danger w-100">
+               Logout
+            </a>
+        </li>
 
-<a href="../logout.php"
-class="btn btn-danger w-100">
-
-Logout
-
-</a>
-
-</li>
-
-</ul>
+    </ul>
 
 </div>
