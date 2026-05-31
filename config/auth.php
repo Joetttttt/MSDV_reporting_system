f9316a4a35e4d76da085f2bd7d7d9b9bac740928
Joetttttt <wengwohph@gmail.com>
2026-05-31 00:23:35 -0700

    auto update

--- config/auth.php
+++ config/auth.php
@@ -2,10 +2,49 @@
 
 session_start();
 
-if (!isset($_SESSION['user_id'])) {
+include("database.php");
 
-    header("Location: ../auth/login.php");
+if(!isset($_SESSION['user_id'])){
+
+    if(isset($_COOKIE['remember_token'])){
+
+        $token =
+            $_COOKIE['remember_token'];
+
+        $result =
+        mysqli_query(
+            $conn,
+            "SELECT *
+             FROM users
+             WHERE remember_token='$token'"
+        );
+
+        if(mysqli_num_rows($result) > 0){
+
+            $user =
+                mysqli_fetch_assoc($result);
+
+            $_SESSION['user_id']
+                = $user['id'];
+
+            $_SESSION['fullname']
+                = $user['fullname'];
+
+            $_SESSION['role']
+                = $user['role'];
+
+        }
+
+    }
+
+}
+
+if(!isset($_SESSION['user_id'])){
+
+    header(
+        "Location: ../auth/login.php"
+    );
+
     exit();
-
 }
 ?>
