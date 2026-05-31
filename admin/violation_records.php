8802a645ec3638836aa8924b39ff5058b1f1bc96
Joetttttt <wengwohph@gmail.com>
2026-05-31 05:06:45 -0700

    auto update

--- admin/violation_records.php
+++ admin/violation_records.php
@@ -183,7 +183,12 @@ data-bs-target="#viewModal<?php echo $row['report_id']; ?>">
 View
 
 </button>
-
+<button
+class="btn btn-success btn-sm"
+data-bs-toggle="modal"
+data-bs-target="#actionModal<?php echo $row['report_id']; ?>">
+Create Action
+</button>
 
 </td>
 
