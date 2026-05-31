12741a45a3de3ec079021110388a3b808fcb9a98
Joetttttt <wengwohph@gmail.com>
2026-05-31 00:23:23 -0700

    auto update

--- auth/logout.php
+++ auth/logout.php
@@ -2,6 +2,27 @@
 
 session_start();
 
+include("../config/database.php");
+
+if(isset($_COOKIE['remember_token'])){
+
+    $token = $_COOKIE['remember_token'];
+
+    mysqli_query(
+        $conn,
+        "UPDATE users
+         SET remember_token=NULL
+         WHERE remember_token='$token'"
+    );
+
+    setcookie(
+        "remember_token",
+        "",
+        time() - 3600,
+        "/"
+    );
+}
+
 session_destroy();
 
 header("Location: login.php");
