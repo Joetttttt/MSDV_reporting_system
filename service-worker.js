ad8cb2a14a64abc4da6272120dccfafd141e9e24
Joetttttt <wengwohph@gmail.com>
2026-06-04 09:35:32 -0700

    auto update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'mdsv-v1';
-const urlsToCache = ['/mdsv_reporting_system/index.'];
+const urlsToCache = ['/mdsv_reporting_system/index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
