fc90c22e3c8669b6f51b95eadb7dda0a48b5bf69
Joetttttt <wengwohph@gmail.com>
2026-06-04 08:38:24 -0700

    auto update

--- logout.php
+++ logout.php
@@ -3,4 +3,10 @@ session_start();
 session_destroy();
 header("Location: ../index.html");
 exit();
+?><?php
+session_start();
+session_destroy();
+setcookie('remember_user', '', time() - 3600, "/");
+header("Location: ../index.php");
+exit();
 ?>
