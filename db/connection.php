784c608ac49ea4182ef5123e8860850ddac51653
Joetttttt <wengwohph@gmail.com>
2026-06-04 09:02:14 -0700

    auto update

--- db/connection.php
+++ db/connection.php
@@ -1,18 +1,18 @@
 <?php
 $host = 'localhost';
 $dbname = 'mdsv_reporting_system';
-$username = 'root';
-$password = '';
+$db_user = 'root';
+$db_pass = '';
 
 try {
     $pdo = new PDO(
         "mysql:host=$host;dbname=$dbname;charset=utf8",
-        $username,
-        $password
+        $db_user,
+        $db_pass
     );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    die(json_encode(['error' => 'Connection failed: ' . $e->getMessage()]));
+    die('Connection failed: ' . $e->getMessage());
 }
 ?>
