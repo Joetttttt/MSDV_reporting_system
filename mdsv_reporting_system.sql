9fe80797c679014139a1c2f549ee84d5ae83d339
Joetttttt <wengwohph@gmail.com>
2026-06-04 08:54:11 -0700

    auto update

--- mdsv_reporting_system.sql
+++ mdsv_reporting_system.sql
@@ -34,7 +34,7 @@ CREATE TABLE courses (
 );
 
 INSERT INTO courses (name, department_id) VALUES
-('BSIT(Infotech)', 1), ('BIT(Comptech)', 1), ('BIT(e)', 1),
+('BSIT(Infotech)', 1), ('BIT(Comptech)', 1), ('BIT()', 1),
 ('BSED', 2), ('BEED', 2),
 ('BSBA', 3), ('BSA', 3);
 
